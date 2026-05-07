d54ea447c6989a57e5e7995541a48e76a74f99ef
Mesut <mesbol6647@gmail.com>
2026-05-08 00:19:47 +0300

    yeni görsel

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -1,34 +1,72 @@
+// import React from "react";
+
+// const FloatingButtons = () => {
+//   return (
+//     <>
+//       {/* WhatsApp (Sağ altta) */}
+//       <div className="fixed bottom-4 right-4 z-50 animate-bounce">
+//         <a
+//           href="https://wa.me/905357351042"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <img
+//             src="/images/10.png"
+//             alt="WhatsApp"
+//             className="w-16 h-16 rounded-full shadow-lg"
+//           />
+//         </a>
+//       </div>
+
+//       {/* Telefon (Sol altta) */}
+//       <div className="fixed bottom-4 left-4 z-50 animate-bounce ">
+//         <a href="tel:+905357351042">
+//           <img
+//             src="/images/11.png"
+//             alt="Ara"
+//             className="w-16 h-16 rounded-full shadow-lg"
+//           />
+//         </a>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default FloatingButtons;
+
 import React from "react";
 
 const FloatingButtons = () => {
   return (
-    <>
-      {/* WhatsApp (Sağ altta) */}
-      <div className="fixed bottom-4 right-4 z-50 animate-bounce">
-        <a
-          href="https://wa.me/905309123066"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/images/10.png"
-            alt="WhatsApp"
-            className="w-16 h-16 rounded-full shadow-lg"
-          />
-        </a>
-      </div>
+    <div className="fixed right-4 top-1/2 -translate-y-1/2 z-50 flex flex-col gap-4">
+      
+      {/* WhatsApp */}
+      <a
+        href="https://wa.me/905066685090"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="animate-bounce"
+      >
+        <img
+          src="/images/10.png"
+          alt="WhatsApp"
+          className="w-16 h-16 rounded-full shadow-2xl"
+        />
+      </a>
 
-      {/* Telefon (Sol altta) */}
-      <div className="fixed bottom-4 left-4 z-50 animate-bounce ">
-        <a href="tel:+905309123066">
-          <img
-            src="/images/11.png"
-            alt="Ara"
-            className="w-16 h-16 rounded-full shadow-lg"
-          />
-        </a>
-      </div>
-    </>
+      {/* Telefon */}
+      <a
+        href="tel:+905066685090"
+        className="animate-bounce"
+      >
+        <img
+          src="/images/11.png"
+          alt="Ara"
+          className="w-16 h-16 rounded-full shadow-2xl"
+        />
+      </a>
+
+    </div>
   );
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,53 @@
 
 
 
+// import * as React from "react";
+// import Section1 from "../components/Section1";
+// import Footer from "../components/Footer";
+// import Testimonials from "../components/Testimonials";
+// import BlogPosts from "../components/BlogPosts";
+// import Stats from "../components/Stats";
+
+// function Dashboard() {
+  
+//   return (
+//     <>
+//     <div className="relative w-full">
+//   {/* Masaüstü Banner */}
+//   <div className="hidden md:block w-full h-[850px]">
+//     <img
+//       src="/images/pcbanner6.png"
+//       alt="PC Banner"
+//       className="w-full h-full object-cover"
+//     />
+//   </div>
+// {/* Mobil Banner */}
+// <div className="block md:hidden w-full">
+//   <img
+//     src="/images/mobilbanner8.webp"
+//     alt="Mobil Banner"
+//     className="w-full h-auto "
+//   />
+// </div>
+
+// </div>
+
+
+// {/* ALT KISIM BURADA, YUKARI KAYMAMASI İÇİN BOŞLUK VERİLDİ */}
+// <div className="relative z-10 ">
+// {/* <div className="relative z-10 mt-[750px] md:mt-[400px] lg:mt-[750px]"> */}
+//   <Section1 />
+//   <Testimonials />
+//   <BlogPosts />
+//   <Stats />
+//   <Footer />
+// </div>
+
+//     </>
+//   );
+// }
+
+// export default Dashboard;
 import * as React from "react";
 import Section1 from "../components/Section1";
 import Footer from "../components/Footer";
@@ -12,40 +59,36 @@ import BlogPosts from "../components/BlogPosts";
 import Stats from "../components/Stats";
 
 function Dashboard() {
-  
   return (
     <>
-    <div className="relative w-full">
-  {/* Masaüstü Banner */}
-  <div className="hidden md:block w-full h-[850px]">
-    <img
-      src="/images/pcbanner6.png"
-      alt="PC Banner"
-      className="w-full h-full object-cover"
-    />
-  </div>
-{/* Mobil Banner */}
-<div className="block md:hidden w-full">
-  <img
-    src="/images/mobilbanner8.webp"
-    alt="Mobil Banner"
-    className="w-full h-auto "
-  />
-</div>
+      <div className="relative w-full">
+        {/* Masaüstü Banner */}
+        <div className="hidden md:block w-full h-[850px]">
+          <img
+            src="/images/pcbanner6.png"
+            alt="PC Banner"
+            className="w-full h-full object-cover"
+          />
+        </div>
 
-</div>
+        {/* Mobil Banner */}
+        <div className="block md:hidden w-full h-screen overflow-hidden">
+          <img
+            src="/images/mobilbanner8.webp"
+            alt="Mobil Banner"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
 
-
-{/* ALT KISIM BURADA, YUKARI KAYMAMASI İÇİN BOŞLUK VERİLDİ */}
-<div className="relative z-10 ">
-{/* <div className="relative z-10 mt-[750px] md:mt-[400px] lg:mt-[750px]"> */}
-  <Section1 />
-  <Testimonials />
-  <BlogPosts />
-  <Stats />
-  <Footer />
-</div>
-
+      {/* ALT KISIM */}
+      <div className="relative z-10">
+        <Section1 />
+        <Testimonials />
+        <BlogPosts />
+        <Stats />
+        <Footer />
+      </div>
     </>
   );
 }
